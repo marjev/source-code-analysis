9f16dd47f3cfd094ee8cca26b94a30dbd0eb7043
Marko Jevtic <marjevtic@gmail.com>
2023-06-05 14:40:16 +0200

    Set the vector field as a read-only field in Django admin

--- sourcecodes/admin.py
+++ sourcecodes/admin.py
@@ -2,5 +2,8 @@ from django.contrib import admin
 
 from .models import SourceCode
 
+class SourceCodeAdmin(admin.ModelAdmin):
+    readonly_fields = ["vector"]
 
-admin.site.register(SourceCode)
+
+admin.site.register(SourceCode, SourceCodeAdmin)
